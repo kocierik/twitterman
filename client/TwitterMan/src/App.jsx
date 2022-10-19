1977f723a38bb67b726920e81d66e85868963030
geno <gabrigeno@gmail.com>
2022-10-19 16:28:52 +0200

    removed code smell

--- client/TwitterMan/src/App.jsx
+++ client/TwitterMan/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-      const [count, setCount] = useState(0);
       return (
             <div>
                   <h1 className="text-3xl font-bold underline">Hello world!</h1>
