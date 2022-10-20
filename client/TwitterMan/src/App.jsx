94cbc5de3e1e55be8eb36c57b90fcaeec17641df
kocierik <kocierik@gmail.com>
2022-10-20 23:21:16 +0200

    fix: tailwind

--- client/TwitterMan/src/App.jsx
+++ client/TwitterMan/src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import React from "react";
 
 function App() {
       return (
             <div>
-                  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                  <h1 className="bg-green-500">Hello world!</h1>
             </div>
       );
 }
