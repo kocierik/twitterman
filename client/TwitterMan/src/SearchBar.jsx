4a2ba546f2d7784fd388b39a68967c198d287d02
kocierik <kocierik@gmail.com>
2022-10-21 16:07:17 +0200

    feat: add searchBar

---
+++ client/TwitterMan/src/SearchBar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div style={{ flex: '0 1 50%' }} className="flex">
+      <label
+        htmlFor="search-dropdown"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+      >
+        Your Email
+      </label>
+      <select className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200  focus:outline-none  dark:bg-gray-700 dark:hover:bg-gray-600  dark:text-white dark:border-gray-600">
+        <option>Sport</option>
+        <option>Cinema</option>
+        <option>Politics</option>
+      </select>
+      <div className="relative w-full">
+        <input
+          type="search"
+          id="search-dropdown"
+          className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+          placeholder="Search on twitterMan..."
+          required=""
+        />
+        <button
+          type="submit"
+          className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
