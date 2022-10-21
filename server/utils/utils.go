a7eaa012ed7958ce5ba5ae147eaf8111e6e85c03
geno <gabrigeno@gmail.com>
2022-10-21 18:20:12 +0200

    fixed for sonarqube

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -1,20 +1,12 @@
 package utils
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnvVar(key string) string {
-	// load .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 	return os.Getenv(key)
 }
 
