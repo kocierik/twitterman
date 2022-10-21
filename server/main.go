2776fcb33a5d4d7fdccfb3a582e97ee35d20fc7d
geno <gabrigeno@gmail.com>
2022-10-21 10:07:46 +0200

    test and requests ok

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,6 +12,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+type TweetData struct {
+	EditHistoryTweetIds []string `json:"edit_history_tweet_ids"`
+	Id                  string   `json:"id"`
+	Text                string   `json:"text"`
+}
+
+type Tweet struct {
+	DataTmp TweetData `json:"data"`
+}
 
 // Global variables
 var client *http.Client
@@ -27,9 +39,15 @@ func getEnvVar(key string) string {
 	return os.Getenv(key)
 }
 
+func getWelcome(c *gin.Context) {
+	body := "{message:'This is a Go server!'}"
+	c.IndentedJSON(http.StatusOK, body)
+}
+
 func getTweetById(c *gin.Context) {
 	id := c.Param("id") // prendo l'id
 
+	// eseguo la richiesta a twitter
 	endpoint := "https://api.twitter.com/2/tweets/" + id
 	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
 
@@ -40,6 +58,7 @@ func getTweetById(c *gin.Context) {
 	req.Header.Add("Authorization", "Bearer "+getEnvVar("BEARER_TOKEN"))
 
 	response, err := client.Do(req)
+
 	if err != nil {
 		log.Fatalf("Error sending request to API endpoint. %+v", err)
 	}
@@ -48,18 +67,25 @@ func getTweetById(c *gin.Context) {
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
+
+	var result Tweet
+	if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to go struct pointer
+		fmt.Println("Can not unmarshal JSON", err)
+	}
+
 	if err != nil {
 		log.Fatalf("Couldn't parse response body. %+v", err)
 	}
 
-	c.IndentedJSON(http.StatusOK, string(body))
+	c.IndentedJSON(http.StatusOK, result)
 }
 
 func main() {
 	initClient()
 	router := gin.Default()
 
+	router.GET("/welcome", getWelcome)
 	router.GET("/tweet/:id", getTweetById)
 
-	router.Run("localhost:8081")
+	router.Run("localhost:8080")
 }
