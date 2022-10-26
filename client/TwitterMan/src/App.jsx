9c91e5bfc66f459441be1ac8109e15f97b6a596c
kocierik <kocierik@gmail.com>
2022-10-26 14:56:43 +0200

    feat: add route

--- client/TwitterMan/src/App.jsx
+++ client/TwitterMan/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Home from './views/Home'
+import Login from './components/Login'
 import Navbar from './components/Navbar'
-
+import { Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
--- client/TwitterMan/src/main.jsx
+++ client/TwitterMan/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 )
