608b81fe66e69134cf3f3c164e1397731760aea8
kocierik <kocierik@gmail.com>
2022-10-27 12:38:37 +0200

    chore: add number input

--- client/TwitterMan/src/components/Navbar.jsx
+++ client/TwitterMan/src/components/Navbar.jsx
@@ -79,7 +79,7 @@ export default function Navbar() {
                     type="button"
                     className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center mr-2 "
                   >
-                    Login
+                    Sign in
                   </button>
                 </Link>
               )}
--- client/TwitterMan/src/components/Register.jsx
+++ client/TwitterMan/src/components/Register.jsx
@@ -61,6 +61,22 @@ const Register = () => {
               </div>
               <div>
                 <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone
+                </label>
+                <input
+                  type="number"
+                  name="phone"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="3484814543"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
