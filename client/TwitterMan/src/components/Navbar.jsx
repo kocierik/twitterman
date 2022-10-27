471a0cd6375f6a36d8cbc51de8f9ae9b29a85a99
kocierik <kocierik@gmail.com>
2022-10-27 10:30:06 +0200

    fix: mobile menu

--- client/TwitterMan/src/components/Navbar.jsx
+++ client/TwitterMan/src/components/Navbar.jsx
@@ -82,6 +82,28 @@ export default function Example() {
               {isLogged && <ProfileDropDown />}
             </div>
           </div>
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Link to={item.link}>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block px-3 py-2 rounded-md text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
