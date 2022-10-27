98fe549f4d108ff2cd6dc16d4455f3f48a0e4f03
kocierik <kocierik@gmail.com>
2022-10-27 13:03:46 +0200

    chore: add translate

--- client/TwitterMan/src/components/Navbar.jsx
+++ client/TwitterMan/src/components/Navbar.jsx
@@ -79,7 +79,7 @@ export default function Navbar() {
                 <Link to="/login">
                   <button
                     type="button"
-                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center mr-2 "
+                    className=" hover:-translate-y-1 hover:scale-105 duration-300 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center mr-2 "
                   >
                     Sign in
                   </button>
@@ -99,7 +99,7 @@ export default function Navbar() {
                       item.current
                         ? 'bg-gray-900 text-white'
                         : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium'
+                      'block px-3 py-2 rounded-md text-base font-medium '
                     )}
                     aria-current={item.current ? 'page' : undefined}
                   >
