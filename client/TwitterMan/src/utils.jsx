7642b0991bdda314c7b818d177c9a6a6dc1e7ac5
kocierik <kocierik@gmail.com>
2022-11-02 00:14:44 +0100

    chore: add constant

--- client/TwitterMan/src/utils.jsx
+++ client/TwitterMan/src/utils.jsx
@@ -1,1 +1,9 @@
 export const SERVER_URL = 'http://localhost:8080'
+export const TWEET_HASHTAG = '/tweet/hashtag/{0}'
+export const TWEET_USERNAME = '/user/{0}/tweets'
+export const TWEET_ID = '/tweet/id/{0}'
+
+export const stringFormat = (str, ...args) =>
+  str.replace(/{(\d+)}/g, (match, number) =>
+    typeof args[number] != 'undefined' ? args[number] : match
+  )
