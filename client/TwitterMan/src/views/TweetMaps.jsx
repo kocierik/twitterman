5ba929b189c32c8a1853334f0bae3fc2cd380509
kocierik <kocierik@gmail.com>
2022-11-09 11:27:53 +0100

    feat: add map

--- client/TwitterMan/src/views/TweetMaps.jsx
+++ client/TwitterMan/src/views/TweetMaps.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import { Map, Marker } from "pigeon-maps"
 
 const TweetMaps = () => {
   return (
     <>
-      <div>TweetMaps</div>
+      <div className='flex flex-1 flex-col text-white'>
+        <div className='flex flex-1 justify-center text-3xl font-bold p-5'>TweetMaps</div>
+        <div className='p-5'>
+          <Map height={500} defaultCenter={[50.879, 4.6997]} defaultZoom={5}>
+            <Marker width={50} anchor={[44.494887, 11.3426163]} />
+            <Marker width={50} anchor={[43, 21]} />
+          </Map>
+        </div>
+      </div>
     </>
   )
 }
