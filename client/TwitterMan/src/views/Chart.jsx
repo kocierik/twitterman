7fe155383ba512b655be83c2409642269b64e1fe
kocierik <kocierik@gmail.com>
2022-11-10 12:26:11 +0100

    feat: add chart page

--- client/TwitterMan/src/views/Chart.jsx
+++ client/TwitterMan/src/views/Chart.jsx
@@ -1,47 +1,45 @@
-import React, { PureComponent } from 'react'
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts'
-
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-]
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+import React from 'react'
+import BarGraph from '../components/chart/BarGraph'
+import PieGraph from '../components/chart/PieGraph'
+import WordCloud from '../components/chart/WordCloud'
 
 const Chart = () => {
   return (
-    <>
-      <a
+    <div className="flex flex-col  md:flex-row gap-5 flex-1 p-5 justify-center">
+      <div
         href="#"
-        class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Sentiment analysis
         </h5>
-        <p class="flex flex-1">
-          <PieChart width={300} height={400}>
-            <Pie
-              data={data}
-              cx={120}
-              cy={200}
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-            </Pie>
-          </PieChart>
-        </p>
-      </a>
-    </>
+        <div className="flex flex-1 justify-center">
+          <PieGraph />
+        </div>
+      </div>
+      <div
+        href="#"
+        className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          BarGraph analysis
+        </h5>
+        <div className="flex flex-1 justify-center">
+          <BarGraph />
+        </div>
+      </div>
+      <div
+        href="#"
+        className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Word Cloud
+        </h5>
+        <div className="flex flex-1 justify-center p-5">
+          <WordCloud />
+        </div>
+      </div>
+    </div>
   )
 }
 
