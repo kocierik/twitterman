a2e8f891372c11220e1cb9f6498abdfdb8160345
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2022-11-15 18:28:52 +0100

    modifica della grafica

--- client/TwitterMan/src/components/Data.jsx
+++ client/TwitterMan/src/components/Data.jsx
@@ -1,38 +1,40 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './Date_style.css'
+import './format-style.css'
 
-function Data() {
- const [startDate, setStartDate] = useState(new Date());
- const [endDate, setEndDate] = useState(new Date());
+function DataRangePicker() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
- return (
-   <div>
-    <span id="merda">
-     <DatePicker
-       selected={startDate}
-       selectsStart
-       startDate={startDate}
-       endDate={endDate}
-       onChange={date => setStartDate(date)}    
-       className="custom-input"
-     />
-     </span>
-     <span id="merda">
-     <DatePicker
-       selected={endDate}
-       selectsEnd
-       startDate={startDate}
-       endDate={endDate}
-       minDate={startDate}
-       onChange={date => setEndDate(date)}
-       className="custom-input"
-     />
-     </span>
-   </div>
- );
+  return (
+    <div id="dateRangePickerId">
+      <div date-rangepicker class="flex items-center">
+        <div class="relative">
+        <DatePicker
+          selected={startDate}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          onChange={date => setStartDate(date)}    
+          className="custom-input"
+        />
+      </div>
+
+      <span class="mx-4 text-gray-500">to</span>
+      <div class="relative">
+        <DatePicker
+          selected={endDate}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          onChange={date => setEndDate(date)}
+          className="custom-input"
+        />
+      </div>
+    </div>
+  </div>
+  );
 }
-export default Data;
-
-
+export default DataRangePicker;
