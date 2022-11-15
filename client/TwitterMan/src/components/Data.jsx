e3c74ee7a8d7abe45b37ffb193e2f3d078de4b22
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2022-11-15 19:06:53 +0100

    Filter Date senza responsive

--- client/TwitterMan/src/components/Data.jsx
+++ client/TwitterMan/src/components/Data.jsx
@@ -19,6 +19,9 @@ function DataRangePicker() {
           onChange={date => setStartDate(date)}    
           className="custom-input"
         />
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </div>
       </div>
 
       <span class="mx-4 text-gray-500">to</span>
@@ -32,6 +35,9 @@ function DataRangePicker() {
           onChange={date => setEndDate(date)}
           className="custom-input"
         />
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </div>
       </div>
     </div>
   </div>
--- client/TwitterMan/src/components/home/SearchBar.jsx
+++ client/TwitterMan/src/components/home/SearchBar.jsx
@@ -35,14 +35,15 @@ const SearchBar = (props) => {
           onChange={(e) => {
             props.setTextValue(e.target.value)
           }}
-          className="block p-2.5 rounded-xl	indent-3	 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+          className="block p-2.5 rounded-xl	indent-3 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
           placeholder="Search on twitterMan..."
           required
         />}
+
         {props.selectValue === "date" ? 
           <button
           type="submit"
-          className="absolute top-1 right-20 p-2.5 text-sm font-large text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="absolute top-0 right-20 p-2.5 text-sm font-large text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg
               aria-hidden="true"
