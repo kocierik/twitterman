2584222dda141938a0f878acd27a99d2ac9b5e4e
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2022-11-17 10:15:31 +0100

    Modify filter data

--- client/TwitterMan/src/components/home/SearchBar.jsx
+++ client/TwitterMan/src/components/home/SearchBar.jsx
@@ -24,7 +24,6 @@ const SearchBar = (props) => {
         <option value={Const.TWEET_USERNAME}>username</option>
         <option value={Const.TWEET_HASHTAG}>hashtag</option>
         <option value={Const.TWEET_KEYWORD}>keyword</option>
-        <option value="date">date</option>
         {/* <option value={TWEET_ID}>keyword</option> */}{' '}
         {/* TODO: ADD MORE OPTIONS */}
       </select>
@@ -40,10 +39,7 @@ const SearchBar = (props) => {
           placeholder="Search on twitterMan..."
           required
         /> 
-        
-        <If condition={props.selectValue === "date"}>
-          <Then>
-            <Data /> 
+        <Data /> 
             <button
             type="submit"
             className="hover:scale-105 duration-300 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center mr-2"
@@ -51,31 +47,7 @@ const SearchBar = (props) => {
             >
               <b>Search</b>
             </button>
-          </Then>
-          <Else>
-            <button
-              type="submit"
-              className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
-          </Else>
-        </If>
+      
        
       </div>
     </form>
