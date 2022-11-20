70102638b8b515aa44e47c26c0f562b09bfc89d0
geno <gabriele.genovese2@studio.unibo.it>
2022-11-20 17:27:41 +0100

    no more animation on load more button

--- client/TwitterMan/src/views/Home.jsx
+++ client/TwitterMan/src/views/Home.jsx
@@ -84,8 +84,8 @@ const Home = () => {
             <button
               onClick={loadMore}
               type="button"
-              data-aos="zoom-in"
-              data-aos-duration="500"
+              // data-aos="zoom-in"
+              // data-aos-duration="500"
               className="text-white  bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Load more
