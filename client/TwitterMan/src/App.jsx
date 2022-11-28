63b18e2043959308723a1b05dfe016116152a78e
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2022-11-28 11:51:57 +0100

    Add TweetMaps page

--- client/TwitterMan/src/App.jsx
+++ client/TwitterMan/src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom'
 import Login from '../src/views/Login'
 import Register from '../src/views/Register'
 import Footer from './components/Footer'
+import TweetMaps from './views/TweetMaps'
+
 function App() {
   return (
     <>
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/tweetMaps/:id" element={<TweetMaps />} />
         </Routes>
         <Footer />
       </div>
