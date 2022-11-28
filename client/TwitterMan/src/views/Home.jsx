5c391aa9d55f3596ac1a0f9f671c505c9cb8facc
geno <gabriele.genovese2@studio.unibo.it>
2022-11-28 14:44:50 +0100

    tweets visibility fix

--- client/TwitterMan/src/views/Home.jsx
+++ client/TwitterMan/src/views/Home.jsx
@@ -5,67 +5,64 @@ import TweetCard from '../components/home/TweetCard'
 import Charts from '../components/home/Charts'
 import HideTweets from '../components/home/HideTweets'
 import * as Const from '../utils'
-import { If, Then, Else, When, Unless, Switch, Case, Default } from 'react-if';
- 
+
 const Home = () => {
   const [tweetsData, setTweetsData] = useState([])
   const [showTweets, setShowTweets] = useState(true)
-  
- 
+
   const setSentiment = async (tweets) => {
-    let tweetsWithSentiment = [];
-    try{
+    let tweetsWithSentiment = []
+    try {
       var data = []
       for (const k in tweets) {
         data.push({ text: tweets[k].content })
       }
-      let res = await fetch("http://localhost:55555/sentiment",
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          // credentials: 'include',
-          body: JSON.stringify(data)
-        });
+      let res = await fetch('http://localhost:55555/sentiment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // credentials: 'include',
+        body: JSON.stringify(data),
+      })
       res = await res.json()
       tweetsWithSentiment = tweets.map((v, k) => {
         v.sentiment = res[k]
         return v
       })
-    }catch(e){
-      alert(e);
+    } catch (e) {
+      alert(e)
     }
     return tweetsWithSentiment
   }
- 
+
   const searchTweets = async (selectValue, textValue) => {
     try {
       const url = Const.stringFormat(Const.SERVER_URL + selectValue, textValue)
       let res = await fetch(url)
       res = await res.json()
-      setTweetsData(res);
+      setTweetsData(res)
     } catch (e) {
       console.log(e)
     }
   }
- 
+
   const loadMore = async () => {
     try {
       const url = Const.SERVER_URL + Const.TWEET_LOAD
-      let res = await fetch(url);
-      res = await res.json();
-      let sentimentRes = await setSentiment(res);
+      let res = await fetch(url)
+      res = await res.json()
+      let sentimentRes = await setSentiment(res)
       setTweetsData((last) => [...last, ...res])
     } catch (e) {
       console.log(e)
     }
   }
- 
+
   useEffect(() => {
-    console.log(tweetsData);
+    console.log(tweetsData)
   }, [tweetsData])
- 
+
   return (
     <>
       <div
@@ -75,10 +72,13 @@ const Home = () => {
       >
         <div className="flex justify-center">
           <SearchBar searchTweets={searchTweets} />
-          {tweetsData?.length && <HideTweets showTweets={showTweets} setShowTweets={setShowTweets}/>}
+          {tweetsData?.length ? (
+            <HideTweets showTweets={showTweets} setShowTweets={setShowTweets} />
+          ) : null}
         </div>
-        <If condition={showTweets}>
-          <Then>
+
+        {showTweets ? (
+          <div>
             <div className="box-border  m-auto max-w-[75rem] 3xl:max-w-[120rem] columns-1xs sm:columns-2xs md:columns-2 lg:columns-3 xl:columns-3 2xl:columns-3 3xl:columns-5">
               {tweetsData?.map((tweet, i) => {
                 return <TweetCard data={tweet} key={i} />
@@ -97,24 +97,27 @@ const Home = () => {
                 </button>
               )}
             </div>
-          </Then>
-        </If>
+          </div>
+        ) : null}
       </div>
-      {tweetsData?.length && <div className="p-10 dark:bg-gray-900">
-        <div className="flex italic flex-1 italic dark:bg-gray-900 text-white justify-center text-3xl font-bold p-5">
-          Charts
+      {tweetsData?.length && (
+        <div className="p-10 dark:bg-gray-900">
+          <div className="flex italic flex-1 italic dark:bg-gray-900 text-white justify-center text-3xl font-bold p-5">
+            Charts
+          </div>
+          <Charts tweetsData={tweetsData} />
         </div>
-        <Charts tweetsData={tweetsData} />
-      </div>}
-      {tweetsData?.length && 
-      <div className="p-10 dark:bg-gray-900">
-        <div className="flex italic flex-1 italic dark:bg-gray-900 text-white justify-center text-3xl font-bold p-5">
-          TweetMaps
+      )}
+      {tweetsData?.length && (
+        <div className="p-10 dark:bg-gray-900">
+          <div className="flex italic flex-1 italic dark:bg-gray-900 text-white justify-center text-3xl font-bold p-5">
+            TweetMaps
+          </div>
+          <Maps tweetsData={tweetsData} />
         </div>
-        <Maps tweetsData={tweetsData} />
-      </div>}
+      )}
     </>
   )
 }
- 
+
 export default Home
