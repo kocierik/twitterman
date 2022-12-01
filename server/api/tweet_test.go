90c3a751365fad26753d3bc1692dbd36edb70f32
Fabio <faga2703@gmail.com>
2022-12-01 18:36:43 +0100

    test add: TestGetTweetsByHashtag

--- server/api/tweet_test.go
+++ server/api/tweet_test.go
@@ -58,12 +58,37 @@ func TestGetTweetById(t *testing.T) {
 
 func TestGetTweetsByHashtag(t *testing.T) {
 	initApiTest()
+	var dummy = []byte{}
+	response, res := sendTestRequest("GET", "/tweet/hashtag/estremamentespecifico", bytes.NewBuffer(dummy))
 
-	//TODO: Test this
+	mockResponse := `[
+		{
+			"id": "1598367424365137934",
+			"name": "team7",
+			"propic": "https://pbs.twimg.com/profile_images/1581295877624369152/p6aLdDNO_normal.jpg",
+			"timestamp": "2022-12-01T17:24:24.000Z",
+			"content": "ciao pisquani\n\n#estremamentespecifico",
+			"username": "team7test",
+			"public_metrics": {
+				"retweet_count": 0,
+				"reply_count": 0,
+				"like_count": 0,
+				"quote_count": 0
+			},
+			"media": null,
+			"geo": {
+				"id": "",
+				"full_name": "",
+				"coordinates": null
+			}
+		}
+	]`
 
-	//assert.Equal(t, http.StatusOK, res.Code)
-	//assert.Equal(t, ret, result)
-	assert.Equal(t, 1, 1)
+	result := utils.UnmarshalToJson[[]utils.Tweet](response)
+	tmpMock := utils.UnmarshalToJson[[]utils.Tweet]([]byte(mockResponse))
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, tmpMock, result)
 }
 
 /*
