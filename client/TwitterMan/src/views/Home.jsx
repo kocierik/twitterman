63c5aa461bfdbe602cfac3ec87a9f368299b1502
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-12-01 21:45:40 +0100

    Paginazione completa

--- client/TwitterMan/src/views/Home.jsx
+++ client/TwitterMan/src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Maps from '../components/home/Maps'
 import SearchBar from '../components/home/SearchBar'
-import TweetCard from '../components/home/TweetCard'
+import TweetsSection from '../components/home/TweetsSection'
 import Charts from '../components/home/Charts'
 import HideTweets from '../components/home/HideTweets'
 import * as Const from '../utils'
@@ -13,6 +13,7 @@ const Home = () => {
   const [sentimentIcon, setSentimentIcon] = useState(null)
   const [sliderValue, setSliderValue] = useState(null)
   const [frequencyValue, setFrequencyValue] = useState(1440)
+  const [rfp, setRfp] = useState(15)
 
   const fetchSentiment = async (tweets) => {
     let tweetsWithSentiment = []
@@ -21,7 +22,7 @@ const Home = () => {
       for (const k in tweets) {
         data.push({ text: tweets[k].content })
       }
-      let res = await fetch('http://localhost:5555/sentiment', {
+      let res = await fetch('http://localhost:5556/sentiment', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -43,7 +44,7 @@ const Home = () => {
     try {
       const url = Const.stringFormat(
         Const.SERVER_URL + selectValue,
-        15 + textValue + formattedDates
+        rfp, textValue + formattedDates
       )
       let res = await fetch(url)
       res = await res.json()
@@ -81,7 +82,7 @@ const Home = () => {
 
   const loadMore = async () => {
     try {
-      const url = Const.stringFormat(Const.SERVER_URL + Const.TWEET_LOAD, 15)
+      const url = Const.stringFormat(Const.SERVER_URL + Const.TWEET_LOAD, rfp)
       let res = await fetch(url)
       res = await res.json()
       if (res) {
@@ -117,25 +118,8 @@ const Home = () => {
             <HideTweets showTweets={showTweets} setShowTweets={setShowTweets} />
           ) : null}
         </div>
-        {showTweets ? (
-          <div>
-            <div className="box-border  m-auto max-w-[75rem] 3xl:max-w-[120rem] columns-1xs sm:columns-2xs md:columns-2 lg:columns-3 xl:columns-3 2xl:columns-3 3xl:columns-5">
-              {tweetsDataFiltered?.map((tweet, i) => {
-                return <TweetCard data={tweet} key={i} />
-              })}
-            </div>
-            <div className="flex  justify-center">
-              {tweetsData?.length > 0 && (
-                <button
-                  onClick={loadMore}
-                  type="button"
-                  className="text-white  bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Load more
-                </button>
-              )}
-            </div>
-          </div>
+        {showTweets && tweetsDataFiltered?.length ? (
+          <TweetsSection tweetsDataFiltered={tweetsDataFiltered} loadMore={loadMore} rfp={rfp} setRfp={setRfp} />
         ) : null}
       </div>
 
