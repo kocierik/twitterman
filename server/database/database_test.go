6a4fc295459679eb5e33234e427f754e2a9c2df2
geno <gabriele.genovese2@studio.unibo.it>
2022-12-03 17:59:41 +0100

    test

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -1,54 +1,43 @@
 package database
 
-import (
-	"context"
-	"testing"
+// func TestDatabaseConnect(t *testing.T) {
+// 	Connect()
+// 	defer client.Disconnect(ctx)
 
-	"git.hjkl.gq/team7/twitterman/server/utils"
+// 	err := client.Ping(context.Background(), nil)
+// 	assert.Equal(t, err, nil)
+// }
 
-	"github.com/stretchr/testify/assert"
+// func TestDatabaseDisconnect(t *testing.T) {
+// 	Connect()
+// 	Disconnect()
+// 	err := client.Ping(ctx, nil)
+// 	assert.NotEqual(t, err, nil)
+// }
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+// func TestInsertandGetUser(t *testing.T) {
+// 	InitDbTest()
 
-func TestDatabaseConnect(t *testing.T) {
-	Connect()
-	defer client.Disconnect(ctx)
+// 	// It insert the user
+// 	InsertUser("gianni@gianni", "gianni", "gianni", []string{})
 
-	err := client.Ping(context.Background(), nil)
-	assert.Equal(t, err, nil)
-}
+// 	// It returns an error if the email doesn't exist
+// 	_, err := GetUserByEmail("no-user-with-this-email")
+// 	assert.NotEqual(t, err, nil)
 
-func TestDatabaseDisconnect(t *testing.T) {
-	Connect()
-	Disconnect()
-	err := client.Ping(ctx, nil)
-	assert.NotEqual(t, err, nil)
-}
+// 	// It returns the user if the mail does exist
+// 	userEmail, err := GetUserByEmail("gianni@gianni")
+// 	utils.TestError(err, "Test GetUserByEmail function error")
+// 	assert.Equal(t, userEmail.Email, "gianni@gianni")
 
-func TestInsertandGetUser(t *testing.T) {
-	InitDbTest()
+// 	// It returns an error if the ID doesn't exist
+// 	_, err = GetUserById(primitive.NewObjectID())
+// 	assert.NotEqual(t, err, nil)
 
-	// It insert the user
-	InsertUser("gianni@gianni", "gianni", "gianni", []string{})
+// 	// It returns the user if the id exist
+// 	userId, err := GetUserById(userEmail.ID)
+// 	utils.TestError(err, "Test GetUserById function error")
+// 	assert.Equal(t, userEmail, userId)
 
-	// It returns an error if the email doesn't exist
-	_, err := GetUserByEmail("no-user-with-this-email")
-	assert.NotEqual(t, err, nil)
-
-	// It returns the user if the mail does exist
-	userEmail, err := GetUserByEmail("gianni@gianni")
-	utils.TestError(err, "Test GetUserByEmail function error")
-	assert.Equal(t, userEmail.Email, "gianni@gianni")
-
-	// It returns an error if the ID doesn't exist
-	_, err = GetUserById(primitive.NewObjectID())
-	assert.NotEqual(t, err, nil)
-
-	// It returns the user if the id exist
-	userId, err := GetUserById(userEmail.ID)
-	utils.TestError(err, "Test GetUserById function error")
-	assert.Equal(t, userEmail, userId)
-
-	Disconnect()
-}
+// 	Disconnect()
+// }
