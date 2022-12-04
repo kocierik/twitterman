167087bb5709e127e858cf3401e587a5558160e5
geno <gabriele.genovese2@studio.unibo.it>
2022-12-04 12:50:58 +0100

    removed TestInsertandGetUser

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"testing"
 
-	"git.hjkl.gq/team7/twitterman/server/utils"
 	"github.com/go-playground/assert/v2"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestDatabaseConnect(t *testing.T) {
@@ -24,29 +22,29 @@ func TestDatabaseDisconnect(t *testing.T) {
 	assert.NotEqual(t, err, nil)
 }
 
-func TestInsertandGetUser(t *testing.T) {
-	InitDbTest()
+// func TestInsertandGetUser(t *testing.T) {
+// 	InitDbTest()
 
-	// It insert the user
-	InsertUser("gianni@gianni", "gianni", "gianni", []string{})
+// 	// It insert the user
+// 	InsertUser("gianni@gianni", "gianni", "gianni", []string{})
 
-	// It returns an error if the email doesn't exist
-	_, err := GetUserByEmail("no-user-with-this-email")
-	assert.NotEqual(t, err, nil)
+// 	// It returns an error if the email doesn't exist
+// 	_, err := GetUserByEmail("no-user-with-this-email")
+// 	assert.NotEqual(t, err, nil)
 
-	// It returns the user if the mail does exist
-	userEmail, err := GetUserByEmail("gianni@gianni")
-	utils.TestError(err, "Test GetUserByEmail function error")
-	assert.Equal(t, userEmail.Email, "gianni@gianni")
+// 	// It returns the user if the mail does exist
+// 	userEmail, err := GetUserByEmail("gianni@gianni")
+// 	utils.TestError(err, "Test GetUserByEmail function error")
+// 	assert.Equal(t, userEmail.Email, "gianni@gianni")
 
-	// It returns an error if the ID doesn't exist
-	_, err = GetUserById(primitive.NewObjectID())
-	assert.NotEqual(t, err, nil)
+// 	// It returns an error if the ID doesn't exist
+// 	_, err = GetUserById(primitive.NewObjectID())
+// 	assert.NotEqual(t, err, nil)
 
-	// It returns the user if the id exist
-	userId, err := GetUserById(userEmail.ID)
-	utils.TestError(err, "Test GetUserById function error")
-	assert.Equal(t, userEmail, userId)
+// 	// It returns the user if the id exist
+// 	userId, err := GetUserById(userEmail.ID)
+// 	utils.TestError(err, "Test GetUserById function error")
+// 	assert.Equal(t, userEmail, userId)
 
-	Disconnect()
-}
+// 	Disconnect()
+// }
