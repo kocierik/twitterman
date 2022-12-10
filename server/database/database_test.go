26c853be3b156eb16103563168a5a6e2ca5eab28
geno <gabriele.genovese2@studio.unibo.it>
2022-12-10 13:06:11 +0100

    uncomment TestInsertandGetUser

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"git.hjkl.gq/team7/twitterman/server/utils"
 	"github.com/go-playground/assert/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var testUsername string = "aldo"
@@ -26,32 +28,32 @@ func TestDatabaseDisconnect(t *testing.T) {
 	assert.NotEqual(t, err, nil)
 }
 
-// func TestInsertandGetUser(t *testing.T) {
-//InitDbTest()
+func TestInsertandGetUser(t *testing.T) {
+	InitDbTest()
 
-// It insert the user
-//InsertUser("gianni@gianni", "gianni", "gianni", []utils.TweetsFolder{})
+	// It insert the user
+	InsertUser("gianni@gianni", "gianni", "gianni", []utils.TweetsFolder{})
 
-// It returns an error if the email doesn't exist
-//_, err := GetUserByEmail("no-user-with-this-email")
-//assert.NotEqual(t, err, nil)
+	// It returns an error if the email doesn't exist
+	_, err := GetUserByEmail("no-user-with-this-email")
+	assert.NotEqual(t, err, nil)
 
-// It returns the user if the mail does exist
-//userEmail, err := GetUserByEmail("gianni@gianni")
-//utils.TestError(err, "Test GetUserByEmail function error")
-//assert.Equal(t, userEmail.Email, "gianni@gianni")
+	// It returns the user if the mail does exist
+	userEmail, err := GetUserByEmail("gianni@gianni")
+	utils.TestError(err, "Test GetUserByEmail function error")
+	assert.Equal(t, userEmail.Email, "gianni@gianni")
 
-// It returns an error if the ID doesn't exist
-//_, err = GetUserById(primitive.NewObjectID())
-//assert.NotEqual(t, err, nil)
+	// It returns an error if the ID doesn't exist
+	_, err = GetUserById(primitive.NewObjectID())
+	assert.NotEqual(t, err, nil)
 
-// It returns the user if the id exist
-// userId, err := GetUserById(userEmail.ID)
-// utils.TestError(err, "Test GetUserById function error")
-// assert.Equal(t, userEmail, userId)
+	// It returns the user if the id exist
+	userId, err := GetUserById(userEmail.ID)
+	utils.TestError(err, "Test GetUserById function error")
+	assert.Equal(t, userEmail, userId)
 
-//Disconnect()
-// }
+	Disconnect()
+}
 
 func TestFolderUsage(t *testing.T) {
 	InitDbTest()
