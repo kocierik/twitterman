02e7df31d7f4ffda89605a18a4ef5c5cc14b18a8
takenX10 <frau.alessandro01@gmail.com>
2022-12-10 13:54:30 +0100

    added getmail endpoint

--- server/api/init.go
+++ server/api/init.go
@@ -29,6 +29,7 @@ var endpointList = []endpoint{
 	{"/register", registerApi, "POST"},
 	{"/is_logged", isLoggedIn, "GET"},
 	{"/logout", logout, "GET"},
+	{"/getmail", getMailFromJwt, "GET"},
 }
 
 func cORSMiddleware() gin.HandlerFunc {
