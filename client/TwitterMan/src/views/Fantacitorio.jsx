c45b60d904a45f80ba5b5cfb099a667e633f1826
takenX10 <frau.alessandro01@gmail.com>
2022-12-10 15:21:17 +0100

    added scoreboard to fantacitorio

--- client/TwitterMan/src/views/Fantacitorio.jsx
+++ client/TwitterMan/src/views/Fantacitorio.jsx
@@ -1,15 +1,75 @@
+import { plPL } from '@mui/x-date-pickers/locales';
 import { useEffect, useState } from 'react'
+import { SERVER_URL } from '../utils'
 
+const searchTweets = async (selectValue, textValue, formattedDates) => {
+    let final = null;
+    try {
+        let res = await fetch(`${SERVER_URL}${selectValue}90${textValue}${formattedDates}`)
+        res = await res.json()
+        if (!res.message) {
+            res.forEach((tw) => {
+                var s = tw.content + ' '
+                tw.content = s.replace(/(http|https)(.*?)( )/g, '')
+            })
+            final = res;
+        } else {
+            alert(res ? res.message : 'Tweets not found')
+        }
+    } catch (e) {
+        console.log(e)
+    }
+    return final;
+}
 
+function formatDate() {
+    let today = new Date();
+    let before = new Date();
+    before.setDate(today.getDate() - 6);
+    return `${before.toISOString()}/${today.toISOString()}`;
+}
 
 function Fantacitorio() {
-    const [punteggi, setPunteggi] = useState([{"score":100, "name":"giorgia meloni", "position":1}]);
+    const [punteggi, setPunteggi] = useState([]);
 
-    async function trovaPunti(){
-
+    async function trovaPunti() {
+        let final = [];
+        let tw = await searchTweets(`/tweet/`, '/user/fanta_citorio', `/date/${formatDate()}`);
+        for (let t of tw) {
+            let splitted = t.content.split("\n")
+            for (let s of splitted) {
+                if (s.includes("PUNTI")) {
+                    try {
+                        let divided = s.split(" - ");
+                        let punti = parseInt(divided[0].split(" ")[0])
+                        let nome = divided[1]
+                        let flag = false;
+                        for (let p of final) {
+                            if (p.name == nome) {
+                                flag = true;
+                                p.score += punti;
+                            }
+                        }
+                        if (!flag){
+                            final.push({"name":nome, "score":punti})
+                        }
+                    } catch (err) {
+                        console.log(err);
+                    }
+                }
+            }
+        }
+        final = final.sort((a, b)=>{
+            return a.score < b.score;
+        });
+        console.log(final);
+        for(let i in final){
+            final[i]["position"] = parseInt(i)+1;
+        }
+        setPunteggi(final);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         trovaPunti();
     }, [])
 
@@ -21,14 +81,16 @@ function Fantacitorio() {
             <div className="text-center flex justify-center p-5">
                 <table>
                     <thead>
-                        <th className='p-3'>Posizione</th>
-                        <th className='p-3'>Politico</th>
-                        <th className='p-3'>Punteggio</th>
+                        <tr>
+                            <th className='p-3'>Posizione</th>
+                            <th className='p-3'>Politico</th>
+                            <th className='p-3'>Punteggio</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        {punteggi.map((p)=>{
+                        {punteggi.map((p) => {
                             return (
-                                <tr>
+                                <tr key={p.position}>
                                     <td className='p-3'>{p.position}</td>
                                     <td className='p-3'>{p.score}</td>
                                     <td className='p-3'>{p.name}</td>
