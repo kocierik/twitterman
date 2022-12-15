0e534f0c27f32a03cee69dc78db6165430fbb7cf
Fabio <faga2703@gmail.com>
2022-12-15 20:52:50 +0100

    tested a good portion of the API

--- server/api/login_test.go
+++ server/api/login_test.go
@@ -149,8 +149,16 @@ func TestSession(t *testing.T) {
 	// test logout
 	out, _ := sendTestReqAuth("GET", "/isLogged", bytes.NewBuffer(nil), cookie)
 	assert.Equal(t, `{"success":true}`, string(out))
+
 	out, _ = sendTestReqAuth("GET", "/getmail", bytes.NewBuffer(nil), cookie)
 	assert.Equal(t, `{"email":"aldo@aldo","success":true}`, string(out))
+
+	tmpVal := cookie.Value
+	cookie.Value = "fvasbf"
+	out, _ = sendTestReqAuth("GET", "/getmail", bytes.NewBuffer(nil), cookie)
+	assert.Equal(t, `{"message":"token not correct","success":false}`, string(out))
+	cookie.Value = tmpVal
+
 	out, _ = sendTestReqAuth("GET", "/logout", bytes.NewBuffer(nil), cookie)
 	assert.Equal(t, `{"success":true}`, string(out))
 	out, _ = sendTestRequest("GET", "/isLogged", bytes.NewBuffer(nil))
--- server/api/user_test.go
+++ server/api/user_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"git.hjkl.gq/team7/twitterman/server/database"
@@ -44,15 +45,39 @@ func TestUpdateUser(t *testing.T) {
 	assert.Equal(t, body.Password, usr.Password)
 }
 
-/*
 func TestGetUserInfo(t *testing.T) {
 	initApiTest()
-	response, res := sendTestRequest("GET", "/user/aodawo", nil)
+	database.InitDbTest()
+	database.InsertUser(testUser.Email, testUser.Username, testUser.Password, []utils.TweetsFolder{})
 
-	tmpMock := `{"message":"Problem fetching the user","success":false}`
+	cookie := getTestCookie(testUser.Email, testUser.Password)
+	response, _ := sendTestReqAuth("GET", "/user", nil, cookie)
+	var userRep utils.User
+	json.Unmarshal([]byte(response), &userRep)
+	fmt.Println(userRep)
+	assert.Equal(t, testUser.Username, userRep.Username)
 
-	assert.Equal(t, http.StatusBadRequest, res.Code)
-	assert.Equal(t, string(response), tmpMock)
+}
 
-	//TODO: test user esistente
-}*/
+func TestSavingTweets(t *testing.T) {
+	initApiTest()
+	database.InitDbTest()
+	defer database.Disconnect()
+	database.InsertUser(testUser.Email, testUser.Username, testUser.Password, []utils.TweetsFolder{})
+	cookie := getTestCookie(testUser.Email, testUser.Password)
+
+	response, _ := sendTestReqAuth("POST", "/user/folder/sium/add/123456789", nil, cookie)
+	usr, _ := database.GetUserByEmail(testUser.Email)
+	fmt.Println(usr)
+	assert.Equal(t, "123456789", usr.SavedFolders[0].Tweets[0])
+
+	response, _ = sendTestReqAuth("POST", "/user/folder/delete/sium", nil, cookie)
+	usr, _ = database.GetUserByEmail(testUser.Email)
+	assert.Equal(t, 0, len(usr.SavedFolders))
+
+	response, _ = sendTestReqAuth("POST", "/user/folder/create/rosalinda", nil, cookie)
+	usr, _ = database.GetUserByEmail(testUser.Email)
+	assert.Equal(t, 1, len(usr.SavedFolders))
+	fmt.Println(response)
+
+}
